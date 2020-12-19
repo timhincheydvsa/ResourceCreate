14eb6f44079e23388aeb1e95b398cc477c443331
cemvarol <cem.varol@outlook.com>
2020-12-19 12:21:23 +0000

    Add files via upload

---
+++ TASKBAR opts.bat
@@ -0,0 +1,7 @@
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V TaskbarSmallIcons /T REG_DWORD /D 1 /F
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /V MMTaskbarEnabled /T REG_dWORD /D 1 /F
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /V MMTaskbarGlomLevel /T REG_dWORD /D 2 /F
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V TaskbarGlomLevel /T REG_DWORD /D 2 /F
+
+taskkill /f /im explorer.exe
+start explorer.exe
